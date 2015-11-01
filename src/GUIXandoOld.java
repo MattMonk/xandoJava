3aa86a615529d0b4e5b6f49c2a99f278eca3b125
MattMonk <monk.matthew@gmail.com>
2015-11-01 16:10:12 +0000

    Added a semi-colon to GUIXandoOld @ line 76

--- src/GUIXandoOld.java
+++ src/GUIXandoOld.java
@@ -73,7 +73,7 @@ public class GUIXandoOld extends JFrame implements ActionListener
 	
 	final int[][] lines = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // rows
 							{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // columns
-							{0, 4, 8}, {2, 4, 6}} // diagonals
+							{0, 4, 8}, {2, 4, 6}}; // diagonals
 							
 	
 	public String checkWin(int a, int b, int c)
